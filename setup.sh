57f4b42e01ac66a78fb4a18860bea8784a41178a
Francisco Altimari <francisco@altimari.nl>
2024-07-04 17:50:54 +0200

    Add symlinks to bash profile and aliases

--- setup.sh
+++ setup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-echo 'Still WIP, sorry :-('
+
+echo "$0 will now create symbolic links"
 
 cd ~
 
-echo 'Create .gitconfig'
+echo 'Creating .gitconfig'
 if [ ! -L .gitconfig ]; then
     ln -s .scripts/.gitconfig .gitconfig
 fi
+
+echo 'Creating .bash_profile'
+if [ ! -L .bash_profile  ]; then
+    ln -s .scripts/.bash_profile .bash_profile
+fi
+
+echo 'Creating .bash_aliases'
+if [ ! -L .bash_aliases ]; then
+    ln -s .scripts/.bash_aliases .bash_aliases
+fi
+
+# TODO: make automatic setup of Konsole theme from scripts
 
