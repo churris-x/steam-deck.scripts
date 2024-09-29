753e408e22aca2c1de2eb616974a8e319bb5d8ba
Francisco Altimari <francisco@altimari.nl>
2024-09-30 00:36:17 +0200

    Add descriptions to binaries

--- install.sh
+++ install.sh
@@ -8,14 +8,14 @@ echo
 
 binaries=(
     # Build tools
-    meson
-    ninja
-    cmake
-    gcc
-    clang
+    meson                       # High productivity build system
+    ninja                       # Small build system with a focus on speed
+    cmake                       # A cross-platform open-source make syste
+    gcc                         # The GNU Compiler Collection - C and C++ frontends
+    clang                       # C language family frontend for LLVM
 
     # Utils
-    bat
+    bat                         # Cat clone with syntax highlighting and git integration
 )
 
 # Generate descriptions with:
